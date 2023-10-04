1bcc3ec0ca4fdb2920e0944e84a90040df6b90d6
castilloxavie <xcastillovaron93@gmail.com>
2023-10-03 20:50:57 -0500

    setings in repiar-services

--- src/repairs/repairs.services.js
+++ src/repairs/repairs.services.js
@@ -23,8 +23,8 @@ export class RepairsServices {
         })
     }
 
-    async updateRepair(repair, data){
-        return await repair.update(data)
+    async updateRepair(repair){
+        return await repair.update({status:"completed"})
     }
 
 
